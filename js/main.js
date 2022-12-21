2a7a86e253d3ac812b9a534c537e879040a8fba5
dbabsoniv <dbabsoniv@gmail.com>
2022-12-20 21:40:18 -0800

    9:40 FULL PORTFOLIO UPLOAD

---
+++ js/main.js
@@ -0,0 +1,38 @@
+
+var newWindow = document.createElement("div");
+newWindow.setAttribute("id","newWindow");
+newWindow.setAttribute("width", window.innerWidth);
+newWindow.setAttribute("height", window.innerHeight);
+
+var header = document.getElementById("header");
+var buttons = document.getElementById("buttonContainer")
+
+window.onload = setPortfolio();
+
+function setPortfolio() {
+    console.log(window.innerWidth);
+    console.log(window.innerHeight);
+    //if (detectMobile()) {drawMobile();}
+
+    const options = document.getElementsByClassName("option");
+    for(let i=0; i<options.length; i++) {
+        options[i].onclick = function() {
+            optionSelect(options[i]);
+        }
+    }
+}
+
+function optionSelect(option) {
+
+    switch (option.getAttribute("id")) {
+        case 'resume':
+            window.location.href = "data/resume2022PDF.pdf";
+            break;
+        case 'mapwork':
+            window.location.href = "mapwork.html";
+            break;
+        case 'personal':
+            window.location.href = "personal.html";
+            break;
+    }
+}
